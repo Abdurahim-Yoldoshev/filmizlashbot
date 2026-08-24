c0dd1ebd24cc2d1c3cede7cdc4cd9a70f36bb26c
Abdurahim-Yoldoshev <yoldoshevcoder@gmail.com>
2026-08-24 15:57:19 +0500

    fix: start express server immediately to satisfy Render port check

--- index.js
+++ index.js
@@ -23,30 +23,11 @@ const { initSettingsTable } = require('./base/models/settings.model.js');
 
 app.use(express.json());
 const dev = async () => {
-    await initSettingsTable();
-    await initUserTable();
-    await initMoviesTable();
-    await initChannelTable();
-    await initSubscriptionTable();
-    await initSeriesTable();
-    await initPromocodesTable();
-    
-    startChecker(); // 30 soniyalik tekshirgichni ishga tushiramiz
-    const { startAutoPromo } = require('./src/automatic/channels/promo');
-    startAutoPromo(); // Har kungi random promokod tarqatish tizimi
-    const { startUserbot } = require('./src/userbot/client');
-    startUserbot(); // Saqlangan session bilan userbotni ishga tushirish
-    
-    app.get('/', (req, res) => {
-        res.send('Bot is running!');
-    });
-
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, '0.0.0.0', () => {
         console.log(`Server is running on port ${PORT}`);
         
         // Render bepul tarifida 15 daqiqadan so'ng uxlab qolmasligi uchun Self-Ping
-        // Render avtomatik RENDER_EXTERNAL_URL beradi, shuni tekshiramiz:
         const renderUrl = process.env.RENDER_EXTERNAL_URL;
         if (renderUrl) {
             const axios = require('axios');
@@ -57,9 +38,29 @@ const dev = async () => {
                     console.error(`[Uyg'otish] Xato:`, err.message);
                 });
             }, 10 * 60 * 1000); // Har 10 daqiqada ping yuboriladi
-        } else {
-            console.log("RENDER_EXTERNAL_URL topilmadi. Agar Render'da bo'lsangiz, bu o'zgaruvchini URLingiz bilan qo'shing.");
         }
+    });
+
+    try {
+        await initSettingsTable();
+        await initUserTable();
+        await initMoviesTable();
+        await initChannelTable();
+        await initSubscriptionTable();
+        await initSeriesTable();
+        await initPromocodesTable();
+        
+        startChecker(); // 30 soniyalik tekshirgichni ishga tushiramiz
+        const { startAutoPromo } = require('./src/automatic/channels/promo');
+        startAutoPromo(); // Har kungi random promokod tarqatish tizimi
+        const { startUserbot } = require('./src/userbot/client');
+        startUserbot(); // Saqlangan session bilan userbotni ishga tushirish
+    } catch (e) {
+        console.error("Initialization error:", e);
+    }
+    
+    app.get('/', (req, res) => {
+        res.send('Bot is running!');
     });
 }
 
